57133ffa28c6b5dba2660f1dff33ea35e3fef998
blazern <danil.garmanov@protonmail.com>
2022-02-03 12:49:57 +0300

    Add UserContributionType.LEGACY_PRODUCT_EDITED

--- src/model/UserContributionType.kt
+++ src/model/UserContributionType.kt
@@ -6,7 +6,8 @@ enum class UserContributionType(
     PRODUCT_EDITED(1),
     PRODUCT_ADDED_TO_SHOP(2),
     PRODUCT_REPORTED(3),
-    SHOP_CREATED(4);
+    SHOP_CREATED(4),
+    LEGACY_PRODUCT_EDITED(5);
     companion object {
         fun fromPersistentCode(code: Short) = values().find { it.persistentCode == code }
     }
